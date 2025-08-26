0dd1b5701e3c243de7cdf2cd671695a4f9026bcd
Coder178 <88607322+Coder-0178@users.noreply.github.com>
2025-08-25 20:38:31 -0700

    Delete index.d.ts

--- index.d.ts
+++
@@ -1,4 +0,0 @@
-export declare function register(options: {
-    url: string;
-} & RegistrationOptions): Promise<ServiceWorkerRegistration>;
-export declare function registerVite(options: RegistrationOptions): Promise<ServiceWorkerRegistration>;
